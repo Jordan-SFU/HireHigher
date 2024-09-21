e82cd047d57da70a64bb96b80774e094ef12db37
Jordan-SFU <jsm36@sfu.ca>
2024-09-21 11:01:55 -0700

    button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/home";
+import ChatTest from "./pages/chattest";
 import "./styles/App.css";
 
 const router = createBrowserRouter(
@@ -9,7 +10,10 @@ const router = createBrowserRouter(
         {
             path: "/",
             element: <Home />
-
+        },
+        {
+          path: "/chattest",
+          element: <ChatTest />
         }
     ]
 );
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { IconButton } from "@mui/material";
 import StartIcon from '@mui/icons-material/Start';
+import { useNavigate } from "react-router-dom";
 
 import "../styles/Home.css";
 
 export default function Home() {
+    const navigate = useNavigate();
+
     return (
         <>
             <div className='home'>
                 <h1>Unemployment Simulator</h1>
-                <IconButton size="large" color='primary' style={{ background: 'white', borderRadius: '100%' }}>
+                <IconButton size="large" color='primary' style={{ background: 'white', borderRadius: '100%' }} onClick={() => {navigate('/chattest')}}>
                     <StartIcon />
                 </IconButton>
                 <h3>Get Started</h3>
