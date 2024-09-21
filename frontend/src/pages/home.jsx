6f3ecd693b82fe969a0164c91c098172ef3b6ea6
Jordan-SFU <jsm36@sfu.ca>
2024-09-21 10:56:56 -0700

    home page basics

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { IconButton } from "@mui/material";
+import StartIcon from '@mui/icons-material/Start';
+
+import "../styles/Home.css";
 
 export default function Home() {
     return (
-        <div>
-            <h1>unemployment simulator</h1>
-        </div>
+        <>
+            <div className='home'>
+                <h1>Unemployment Simulator</h1>
+                <IconButton size="large" color='primary' style={{ background: 'white', borderRadius: '100%' }}>
+                    <StartIcon />
+                </IconButton>
+                <h3>Get Started</h3>
+                <div className="authors">
+                    <p>Jordan McKenzie, Nicholas Chan, Riley Su, Bjorn Flamminman</p>
+                </div>
+            </div>
+        </>
     );
 }
