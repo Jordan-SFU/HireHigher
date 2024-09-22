8e8ef8d045629447a1fb70d79ec7d74a22ed06bc
Nicholas Chan <nicholasch.an@outlook.com>
2024-09-22 01:56:16 -0700

    backend

--- backend/helpers/linkedScrape.py
+++ backend/helpers/linkedScrape.py
@@ -4,11 +4,13 @@ driver = webdriver.Chrome()
 
 email = "SpecialSongHacks@gmail.com"
 password = "superballs"
-def ScrapeLinkedIn(email):
+def ScrapeLinkedIn(text):
     try:
         actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-        person = Person("https://www.linkedin.com/in/nickolas-chan-7943451a0/", driver=driver)
+        person = Person(text, driver=driver)
         printstr = (str(person.about) if str(person.about) != "[]" else "") + " " + (str(person.experiences) if str(person.experiences) != "[]" else "") + " " +(str(person.educations) if str(person.educations) != "[]" else "")
     except:
         return("")
     return(printstr)
+
+print(ScrapeLinkedIn("https://www.linkedin.com/in/ronniekinseymba/"))
