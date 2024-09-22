b74c28b286c18527f9dac1885c97872934b44732
winterkube <theicekubex@gmail.com>
2024-09-22 09:07:57 -0700

    changed home screen css

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 import "../styles/Home.css";
 
 export default function Home() {
+    const items = new Array(100).fill(0); // Create an array to generate the items
+    const items2 = new Array(100).fill(0); // Create an array to generate the items
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,11 +18,43 @@ export default function Home() {
     return (
         <>
             <div className='home'>
-                <h1 className="login-text">HireHigher</h1>
-                <IconButton size="large" color='primary' style={{ background: 'white', borderRadius: '100%' }} onClick={() => {navigate('/setup')}}>
+                <div className="login-container">
+                    <span className="login-text">HireHigher</span>
+                </div>
+
+
+                <IconButton size="large" color='primary' style={{background: 'white', borderRadius: '100%', width: '100px',    // Increase the width
+                    height: '100px',   // Increase the height
+                    fontSize: '50px',   // Increase the icon size
+                    boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)', // Add drop shadow
+                    marginTop: '50px'  // Move the button down
+                         }} onClick={() => {navigate('/setup')}}>
                     <StartIcon />
                 </IconButton>
-                <h3>Get Started</h3>
+                <h3>BEGIN</h3>
+
+                {/* Infinite vertical carousel */}
+                <div className="carousel-container">
+                    <div className="carousel">
+                        {items.map((_, index) => (
+                            <div key={index} className="carousel-item">
+                                <div className="image-animation"></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                {/* Infinite vertical carousel */}
+                <div className="carousel-container2">
+                    <div className="carousel2">
+                        {items2.map((_, index) => (
+                            <div key={index} className="carousel-item">
+                                <div className="image-animation2"></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
                 <div className="authors">
                     <p>Jordan McKenzie, Nicholas Chan, Riley Su, Bjorn Flamminman</p>
                 </div>
